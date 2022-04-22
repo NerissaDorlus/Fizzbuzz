220a5323468cfe5b2ea7d46ecf7d8b0e04c14457
Nerissa Dorlus <nerissadorlus@Tdas-Pro-3.local>
2022-04-22 15:12:24 -0400

    fizzbuzz exercise

---
+++ fizzbuzz.js
@@ -0,0 +1,12 @@
+function dividedByThreeFive(num) {
+  if (num % 3 === 0 && num % 5 === 0) {
+    return "fizzbuzz";
+  } else if (num % 3 === 0) {
+    return "fizz";
+  } else if (num % 5 === 0) {
+    return "buzz";
+  } else {
+    return num;
+  }
+}
+console.log(dividedByThreeFive(25));
